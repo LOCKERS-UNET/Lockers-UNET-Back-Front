cb960afd1ccd577baa355c8176a8d273bc266327
Diego Sanchez <disaru2000@gmail.com>
2026-04-02 14:53:00 -0400

    agregando Layout para admin

---
+++ resources/js/pages/Admin/HomeAdmin.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import LayoutAdmin from '../Layouts/LayoutAdmin.vue';
+
+
+    defineOptions({layout:LayoutAdmin})
+
+</script>
+
+<template>
+
+    <div>
+
+        Menu Admin
+
+    </div>
+
+</template>
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,13 @@ use Illuminate\Support\Facades\Route;
 
 // Route::inertia('/', 'Welcome')->name('home');
 
-
+//Estas rutas deben ir protegidas
 Route::inertia('/inicio','Home')->name('home');
 Route::inertia('/user-profile','User/Profile')->name('user-profile');
 Route::inertia('/edit-profile','User/EditProfile')->name('edit-profile');
+
+//Admin
+Route::inertia('/inicio-admin','Admin/HomeAdmin')->name('home-admin');
 
 Route::get('/login',[LoginController::class,'index'])->name('login');
 Route::post('/login',[LoginController::class,'store']);

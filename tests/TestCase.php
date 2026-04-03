39ddad0e416ec39f208c8ac47ab659983f5879f1
Diego Sanchez <disaru2000@gmail.com>
2026-04-03 15:18:26 -0400

    commit de configuracion TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,15 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Esto le dice a Laravel: "No busques el manifest.json de Vite"
+        // Evita el error 500 que estás recibiendo en GitHub
+        if (class_exists(\Illuminate\Foundation\Vite::class)) {
+            $this->withoutVite();
+        }
+    }
 }

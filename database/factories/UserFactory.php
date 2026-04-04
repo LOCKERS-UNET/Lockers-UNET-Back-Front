0215238f4df53365bb977614ebdf6c1fd95ba2b1
Diego Sanchez <disaru2000@gmail.com>
2026-04-03 22:58:54 -0400

    agregando UserFactory para corregir errores de tests

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'lastname'=>fake()->name(),
+            'card_id'=>fake()->numberBetween(0,99999),
+            'career'=>fake()->name(),
+            'is_admin'=>fake()->boolean(50),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),

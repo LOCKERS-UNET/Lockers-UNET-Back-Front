bce31108560082a58a7f8856706d3e4b1ac056bf
Diego Sanchez <disaru2000@gmail.com>
2026-04-03 22:50:44 -0400

    Corrigiendo errores para los tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -11,8 +12,12 @@ class ExampleTest extends TestCase
 
     public function test_returns_a_successful_response()
     {
-        $response = $this->get(route('home'));
+        $user = User::factory()->create();
 
+        // 2. Usamos actingAs($user) para autenticarnos
+        $response = $this->actingAs($user)->get(route('home'));
+
+        // 3. Ahora sí, el middleware 'auth' nos dejará pasar y devolverá 200 (assertOk)
         $response->assertOk();
     }
 }
